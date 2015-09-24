1dbbca58a89a3e6bd828a70e504ae15f81b5a174
gvelatini <gvelatini@ung.edu>
2015-09-24 13:42:23 -0400

    completed functions in make matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions create a matrix that has an inverse, calculates the inverse matrix, 
+## and caches the inverse matrix so that the inverse does not hyave to be computed ecah time it is needed.
 
-## Write a short comment describing this function
+# The operators <<- and ->> are normally only used in functions, 
+# and cause a search to be made through parent environments for an existing 
+# definition of the variable being assigned. If such a variable is found 
+# (and its binding is not locked) then its value is redefined, otherwise 
+# assignment takes place in the global environment.
+
+## The function makeCacheMatrix  creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  minv <- NULL
+  
+  
+  set <- function(y) {                    ## store the input matrix and sets the inverse to NULL
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x                     ## get the input matrix
+  setinv <- function(inv) minv <<- inv    ## store the inverse matrix
+  getinv <- function() minv               ## get the inverse matrix
+  list(set = set, get = get,              ## put all 4 functions in a list
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve computes the inverse of the 
+## special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+
+  minv <- x$getinv()                     ## get inverse matrix
+  
+  if(!is.null(minv)) {
+    message("getting cached data")       ## check that inverse matrix is not empty
+    return(minv)
+  }
+  mat <- x$get()                         ## get input matrix
+  minv <- solve(mat)                     ## calculate matrix inverse
+  x$setinv(minv)                         ## cache inverse
+  minv
 }
